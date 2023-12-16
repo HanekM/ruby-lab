4949b2192f897f5e63c783b5d04140a003d7adb3
HanekM <bakklazhan3@gmail.com>
2023-12-16 10:53:29 +0200

    ref: code refactor

--- parser.rb
+++ parser.rb
@@ -4,24 +4,9 @@ module Parser_program
     def self.parse_items(condition)
       items = [] 
 
-      # Створюємо агента Mechanize
       agent = Mechanize.new
-      # Отримуємо сторінку за веб-адресою
       page = agent.get(Parser_program.web_address_login)
-      # Знаходимо форму для входу за ідентифікатором
       form = agent.page.search('div.a-box-inner')
-      #puts form
-=begin
-      # Вводимо логін та пароль
-      form.login = Parser_program::User.email
-      form.password = Parser_program::User.password
-
-      # Надсилаємо форму
-      result = form.submit
-
-      page = agent.get(Parser_program.web_address)
-      puts page
-=end
       doc = Nokogiri::HTML(Faraday.get(Parser_program.web_address).body)
       rows = doc.css("div.cli-children") 
 
